e74bdd900cad768dbf1439ac9319947c078abaea
Wiam.LAARABI <votre@email.com>
2024-04-05 12:55:07 +0000

    Second commit

---
+++ Test Suites/DataDrivenDemo - Copy.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DataDrivenDemo - Copy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>633affe0-3165-4cb6-a963-1af0fad264fc</testSuiteGuid>
+   <testCaseLink>
+      <guid>24eb4e36-77e7-468d-b34d-f25564fc0d5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/DataDrivenTestingDemo</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4eb155e2-c3d8-4b8c-bacf-9c274d4a97c1</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>4eb155e2-c3d8-4b8c-bacf-9c274d4a97c1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>7aa754a7-ae22-41d5-836a-58cc5b453972</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4eb155e2-c3d8-4b8c-bacf-9c274d4a97c1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>91b09cae-9820-48a3-bdac-7afd6eff8b19</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
